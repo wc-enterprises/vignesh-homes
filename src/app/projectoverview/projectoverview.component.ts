99778e22c29f5ab50ae0d744204285544c63d102
Arun <arunjothi2599@gmail.com>
2023-12-19 17:55:27 +0530

    specification icon added  in the Project Overview page

--- src/app/projectoverview/projectoverview.component.ts
+++ src/app/projectoverview/projectoverview.component.ts
@@ -98,57 +98,72 @@ export class ProjectoverviewComponent implements OnInit, AfterViewInit {
 
   specifications = [
     {
+      imageSrc: '/assets/structure-spec.svg',
       title: 'STRUCTURE',
       content:
         'Seismic zone III compliant RCC framed structure. Walls will be of AAC blocks',
     },
     {
+      imageSrc: '/assets/door-spec.svg',
       title: 'JOINERIES DOORS',
       content:
         'Doors, Frames Are Made Of 1st Quality Teak Wood With ornamental borders & Shutters Are Wooden Door Pu Varnish Finish With Croft Architectural Anti Brass Fittings',
     },
     {
+      imageSrc: '/assets/window-spec.svg',
       title: 'JOINERIES WINDOWS',
       content:
         'Upvc Framed Windows Catchment Type Saint Gobain Glass And Grill Provision As Per The Architect Design.',
     },
     {
+      imageSrc: '/assets/elevator-spec.svg',
       title: 'ELEVATOR',
       content:
         'Fully Automatic V3f Lift - Six Passenger With SS Finish, Elegant Fall Ceiling and Energy Efficient Light Fixtures.    ',
     },
     {
+      imageSrc: '/assets/painting-spec.svg',
       title: 'PAINTING',
       content:
         'Interior Walls – Two Coats Of JK Wall Care Putty (Or) Asian Wall Putty With Primer One Coats With Two Coats Of Asian Paints Premium Emulsion For Inner Walls.    ',
     },
     {
+      imageSrc: '/assets/flooring-spec.svg',
       title: 'FLOORING',
       content:
         'Living & Dinning Carpet Area Should Be In 24” x 24” Vitrified Tiles Of Somany/ Kajaria/ RAK Or Equal Brands, Bathroom floor Tiles Anti – skid & Wall tiles will be roof height of Somany/ Kajaria/ RAK Or Equal Brands, Entrance Lobby & Staircase with Granite',
     },
-    { title: 'PLUMBING', content: 'CPVC concealed water line' },
     {
+      imageSrc: '/assets/plumbing-spec.svg',
+      title: 'PLUMBING',
+      content: 'CPVC concealed water line',
+    },
+    {
+      imageSrc: '/assets/watersupply-spec.svg',
       title: 'WATER SUPPLY',
       content:
         'One Borewell Of Adequate Depth & Rcc Sump Of 10000 L Capacity for Metro Water & OHT with Booster Pump.',
     },
     {
+      imageSrc: '/assets/security-spec.svg',
       title: 'SECURITY',
       content:
         '24/7 Cctv Surveillance Cameras Control System Provision For The Apartments.',
     },
     {
+      imageSrc: '/assets/terrace-spec.svg',
       title: 'TERRACE',
       content:
         'Provision With A Proper Weathering Course Provided With Cooling Tile. Seating Provision As Per Design.    ',
     },
     {
+      imageSrc: '/assets/electrical-spec.svg',
       title: 'ELECTRICAL',
       content:
         'EB Main 3 Phase Supply With Insulated Copper Conductors Concealed Wiring And Modular Switches Of ELLEYS Brands.',
     },
     {
+      imageSrc: '/assets/floorfinish-spec.svg',
       title: 'FLOOR FINISHES',
       content:
         'Living & Dining - Vitrified tiles, Bedrooms - Vitrified tiles, Kitchen - Anti-skid tiles, Lobby - Indian granite, Stairs - Kota stone',
