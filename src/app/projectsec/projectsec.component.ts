d9ab4c84c3045daa0baf8f0640b69338a28a16ac
Abinwc <abinpaul5598@gmail.com>
2024-03-09 01:00:12 +0530

    changes as per asked in latest feedback round

--- src/app/projectsec/projectsec.component.ts
+++ src/app/projectsec/projectsec.component.ts
@@ -15,7 +15,7 @@ import { ProjectService } from '../common-utils/project-service';
 export class ProjectsecComponent {
   sectionTitle = '';
   sectionDescription = '';
-  // standardProjects: Project[] = [];
+  forSaleProjects: CompletedProject[] = [];
   comingSoonProjects: ComingSoonProject[] = [];
   completedProjects: CompletedProject[] = completedProjects;
   defaultTab: string = 'forsale';
@@ -40,9 +40,10 @@ export class ProjectsecComponent {
         this.sectionTitle = 'FOR SALE PROJECTS';
         this.sectionDescription =
           "Vignesh Homes is pleased to offer a selection of meticulously crafted properties for sale. These are not just buildings; they're future homes and investments. Explore our range of residential and commercial properties, each designed with a focus on quality and aesthetics. Secure your place in a Vignesh Homes creation, where dreams become addresses. Take the first step in owning a piece of architectural excellence.";
-        this.completedProjects = completedProjects.filter(
+        this.forSaleProjects = completedProjects.filter(
           (project) => project.saleTag === '/assets/forsale.svg'
         );
+        this.completedProjects = [];
         this.comingSoonProjects = [];
 
         break;
@@ -54,7 +55,7 @@ export class ProjectsecComponent {
         this.completedProjects = completedProjects.filter(
           (project) => project.saleTag === '/assets/complete.svg'
         );
-        // this.standardProjects = [];
+        this.forSaleProjects = [];
         this.comingSoonProjects = [];
         break;
 
@@ -63,7 +64,7 @@ export class ProjectsecComponent {
         this.sectionDescription =
           'Anticipate our upcoming projects that promise a blend of luxury, innovation, and modern living. Get ready to embark on a new chapter of elegance and sophistication. Stay tuned for more details on these exciting ventures.';
         this.comingSoonProjects = comingSoonProjects;
-        // this.standardProjects = [];
+        this.forSaleProjects = [];
         this.completedProjects = [];
         break;
       default:
